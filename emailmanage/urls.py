95ccbb0a48ce3f53b32584b6b656fe240a6f992f
Junke <wjk1397@sina.com>
2017-12-11 16:06:07 +0800

    邮件回调

--- emailmanage/urls.py
+++ emailmanage/urls.py
@@ -4,12 +4,10 @@ import views
 
 sendemaillist = views.EmailgroupsendlistView.as_view({
         'get': 'list',
-
-})
-sendread = views.EmailgroupsendlistView.as_view({
         'post': 'update',
 })
+
 urlpatterns = [
     url(r'^$', sendemaillist,name='sendemail-list',),
-    url(r'^(?P<pk>\d+)/$', sendread,name='sendemailread',),
+
 ]
