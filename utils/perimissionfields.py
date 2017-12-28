214e85ff34edf2cb5d19bd9840365ea9bd83bf9c
Junke <wjk1397@sina.com>
2017-12-28 14:47:27 +0800

    放开修改微信权限

--- utils/perimissionfields.py
+++ utils/perimissionfields.py
@@ -3,7 +3,7 @@
 
 userpermfield = {
     'usersys.admin_changeuser':[],
-    'usersys.trader_changeuser':['groups', 'createuser', 'createdtime', 'mobile', 'email', 'wechat'],
+    'usersys.trader_changeuser':['groups', 'createuser', 'createdtime', 'mobile', 'email',],
     'usersys.admin_adduser':[],
     'usersys.trader_adduser':[],
     'changeself':['groups', 'createuser', 'createdtime'],
