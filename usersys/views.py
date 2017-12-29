b23bc24938773a050a4725d54ec38efaa6b4486a
Junke <wjk1397@sina.com>
2017-12-29 16:41:19 +0800

    修改用户组报错

--- usersys/views.py
+++ usersys/views.py
@@ -420,23 +420,23 @@ class UserView(viewsets.ModelViewSet):
                             groupid = data.pop('groups', [])
                             if len(groupid) == 1:
                                 try:
-                                    group = Group.objects.get(id=groupid[0])
+                                    groupinstance = Group.objects.get(id=groupid[0])
                                 except Exception:
                                     raise InvestError(code=2007, msg='group bust be an available id')
-                                if group not in user.groups.all():
+                                if groupinstance not in user.groups.all():
                                     if user.has_perm('usersys.as_trader'):
-                                        if not group.has_perm('usersys.as_trader'):
+                                        if Permission.objects.get(codename='as_trader', content_type__app_label='usersys') not in groupinstance.permissions:
                                             if user.trader_relations.all().filter(is_deleted=False).exists():
                                                 raise InvestError(2010, msg='该用户名下有对接投资人，请先处理')
                                             if user.trader_timelines.all().filter(is_deleted=False).exists():
                                                 raise InvestError(2010, msg='该用户名下有对接时间轴，请先处理')
                                     if user.has_perm('usersys.as_investor'):
-                                        if not group.has_perm('usersys.as_investor'):
+                                        if Permission.objects.get(codename='as_investor', content_type__app_label='usersys') not in groupinstance.permissions:
                                             if user.investor_relations.all().filter(is_deleted=False).exists():
                                                 raise InvestError(2010, msg='该用户名下有对接交易师，请先处理')
                                             if user.investor_timelines.all().filter(is_deleted=False).exists():
                                                 raise InvestError(2010, msg='该用户名下有对接时间轴，请先处理')
-                                    data['groups'] = [group.id]
+                                    data['groups'] = [groupinstance.id]
                         else:
                             if request.user == user:
                                 canNotChangeField = perimissionfields.userpermfield['changeself']
