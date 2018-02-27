2ea481ebf8ffc5ce219eaf484db52a296af332fa
Junke <wjk1397@sina.com>
2018-02-27 16:01:46 +0800

    项目服务类型筛选bug修复

--- proj/views.py
+++ proj/views.py
@@ -45,7 +45,7 @@ class ProjectFilter(FilterSet):
     isoverseasproject = RelationFilter(filterstr='isoverseasproject', lookup_method='in')
     industries = RelationFilter(filterstr='industries',lookup_method='in',relationName='project_industries__is_deleted')
     tags = RelationFilter(filterstr='tags',lookup_method='in',relationName='project_tags__is_deleted')
-    service = RelationFilter(filterstr='proj_services', lookup_method='in', relationName='proj_services__is_deleted')
+    service = RelationFilter(filterstr='proj_services__service', lookup_method='in', relationName='proj_services__is_deleted')
     projstatus = RelationFilter(filterstr='projstatus',lookup_method='in')
     country = RelationFilter(filterstr='country',lookup_method='in')
     netIncome_USD_F = RelationFilter(filterstr='proj_finances__netIncome_USD',lookup_method='gte')
