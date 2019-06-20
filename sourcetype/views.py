f71d4c8d1c3cc177ba38cc048049537165ddfc42
junke <wjk1397@gmail.com>
2019-06-20 19:30:52 +0800

    用户菜单减少dashboard和机构管理

--- sourcetype/views.py
+++ sourcetype/views.py
@@ -586,6 +586,8 @@ def getmenulist(user):
         qslist.append(allmenuobj.filter(id__in=[5]))
     if user.has_perm('usersys.as_investor') and not user.is_superuser:
         qslist.append(allmenuobj.filter(id__in=[13]))
+    if not user.has_perm('usersys.as_investor') or user.is_superuser:
+        qslist.append(allmenuobj.filter(id__in=[27, 28, 33]))
     if user.has_perm('usersys.as_trader') and not user.is_superuser:
         qslist.append(allmenuobj.filter(id__in=[12]))
     if user.has_perm('emailmanage.getemailmanage'):
@@ -598,8 +600,6 @@ def getmenulist(user):
         qslist.append(allmenuobj.filter(id__in=[29, 23]))
     if user.has_perm('APILog.manage_userinfolog'):#日志查询
         qslist.append(allmenuobj.filter(id__in=[9]))
-    # if user.has_perm('msg.admin_manageSchedule'):#日程管理
-    #     qslist.append(allmenuobj.filter(id__in=[25]))
     if user.is_superuser:
         qslist.append(allmenuobj.filter(id__in=[17]))
     if user.has_perm('proj.admin_addproj') or user.has_perm('proj.user_addproj'):
@@ -607,7 +607,7 @@ def getmenulist(user):
     if user.has_perm('dataroom.get_companydataroom'):
         qslist.append(allmenuobj.filter(id__in=[31]))
     if user.has_perm('org.export_org'):
-        qslist.append(allmenuobj.filter(id__in=[32]))
-    qslist.append(allmenuobj.filter(id__in=[1, 4, 6, 7, 8, 10, 11, 14, 15, 16, 18, 20, 21, 24, 25, 26, 27, 28, 30, 33]))
+        qslist.append(allmenuobj.filter(id__in=[28, 32]))
+    qslist.append(allmenuobj.filter(id__in=[1, 4, 6, 7, 8, 10, 11, 14, 15, 16, 18, 20, 21, 24, 25, 26, 30]))
     qsres = reduce(lambda x,y:x|y,qslist).distinct().filter(is_deleted=False).order_by('index')
     return WebMenuSerializer(qsres,many=True).data
