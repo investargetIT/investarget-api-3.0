1729514c06f73e38e55352ca19c128c659737291
junke <wjk1397@gmail.com>
2019-07-18 15:37:52 +0800

    全库项目增加融资时间多选

--- mongoDoc/views.py
+++ mongoDoc/views.py
@@ -306,6 +306,10 @@ class ProjectDataView(viewsets.ModelViewSet):
             com_addr = request.GET.get('com_addr')
             finance_date = request.GET.get('finance_date')
             if finance_date:
+                finance_date_list = finance_date.split(',')
+                filters = Q(date__startswith=finance_date)
+                for finance_date in finance_date_list:
+                    filters = filters | Q(date__startswith=finance_date)
                 comid_QS = MergeFinanceData.objects.all().filter(date__startswith=finance_date).values_list('com_id')
                 queryset = queryset.filter(com_id__in=comid_QS)
             if com_addr:
