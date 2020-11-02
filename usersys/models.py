12a4e9e50fcfc62483b710dd8bb353bfbd9ae697
junke <wjk1397@gmail.com>
2020-11-02 14:15:57 +0800

    去除投资人交易师权限验证

--- usersys/models.py
+++ usersys/models.py
@@ -383,11 +383,6 @@ class UserRelation(MyModel):
             raise InvestError(code=8888,msg='requestuser.datasource不匹配')
         if self.traderuser.userstatus_id != 2:
             raise InvestError(code=2022,msg='交易师尚未审核通过，无法建立联系')
-        if not self.is_deleted:
-            if 'usersys.as_investor' in self.investoruser.get_all_permissions() and 'usersys.as_trader' in self.traderuser.get_all_permissions():
-                pass
-            else:
-                raise InvestError(2009, msg='身份类型不符合条件')
         if self.pk:
             userrelation = UserRelation.objects.exclude(pk=self.pk).filter(is_deleted=False,datasource=self.datasource,investoruser=self.investoruser)
         else:
